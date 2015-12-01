362d7aaa2fda9367d0fc18da41e3715e8ef98419
Shaunty Rashidi <shauntyrashidi@Shaunty-Rashidis-Computer.local>
2015-12-01 15:36:25 -0800

    Create constants for chess pieces

---
+++ chess/gameboard.rb
@@ -0,0 +1,16 @@
+class Gameboard
+
+  WHITE_KING = "\u{2654}"
+  WHITE_QUEEN = "\u{2655}"
+  WHITE_ROOK = "\u{2656}"
+  WHITE_BISHOP = "\u{2657}"
+  WHITE_KNIGHT = "\u{2658}"
+  WHITE_PAWN = "\u{2659}"
+  BLACK_KING = "\u{265A}"
+  BLACK_QUEEN = "\u{265B}"
+  BLACK_ROOK = "\u{265C}"
+  BLACK_BISHOP = "\u{265D}"
+  BLACK_KNIGHT = "\u{265E}"
+  BLACK_PAWN = "\u{265F}"
+
+end
