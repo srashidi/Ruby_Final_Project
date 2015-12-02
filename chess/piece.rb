a2738d612925604ad740a00a9daf03c261397bba
Shaunty Rashidi <shauntyrashidi@Shaunty-Rashidis-Computer.local>
2015-12-01 18:04:07 -0800

    Change possible_moves for Piece object to put together related moves

--- chess/piece.rb
+++ chess/piece.rb
@@ -34,83 +34,87 @@ class Piece
 		x,y = position[0],position[1]
 		case @type
 		when :king
-			array << [x.next,y]
-			array << [x.next,y+1]
-			array << [x,y+1]
-			array << [x.prev,y+1]
-			array << [x.prev,y]
-			array << [x.prev,y-1]
-			array << [x,y-1]
-			array << [x.next,y-1]
+			array << [[x.next,y]]
+			array << [[x.next,y+1]]
+			array << [[x,y+1]]
+			array << [[x.prev,y+1]]
+			array << [[x.prev,y]]
+			array << [[x.prev,y-1]]
+			array << [[x,y-1]]
+			array << [[x.next,y-1]]
 		when :queen
-			array << [x.next,y]
-			array << [x.next,y+1]
-			array << [x,y+1]
-			array << [x.prev,y+1]
-			array << [x.prev,y]
-			array << [x.prev,y-1]
-			array << [x,y-1]
-			array << [x.next,y-1]
+			array << [[x.next,y]]
+			array << [[x.next,y+1]]
+			array << [[x,y+1]]
+			array << [[x.prev,y+1]]
+			array << [[x.prev,y]]
+			array << [[x.prev,y-1]]
+			array << [[x,y-1]]
+			array << [[x.next,y-1]]
 			i = 0
 			6.times do
-				array << [array[i][0].next,y]
-				array << [array[i+1][0].next,array[i+1][1]+1]
-				array << [x,array[i+2][1]+1]
-				array << [array[i+3][0].prev,array[i+3][1]+1]
-				array << [array[i+4][0].prev,y]
-				array << [array[i+5][0].prev,array[i+5][1]-1]
-				array << [x,array[i+6][1]-1]
-				array << [array[i+7][0].next,array[i+7][1]-1]
-				i += 8
+				array[0] << [array[0][i][0].next,y]
+				array[1] << [array[1][i][0].next,array[1][i][1]+1]
+				array[2] << [x,array[2][i][1]+1]
+				array[3] << [array[3][i][0].prev,array[3][i][1]+1]
+				array[4] << [array[4][i][0].prev,y]
+				array[5] << [array[5][i][0].prev,array[5][i][1]-1]
+				array[6] << [x,array[6][i][1]-1]
+				array[7] << [array[7][i][0].next,array[7][i][1]-1]
+				i += 1
 			end
 		when :rook
-			array << [x.next,y]
-			array << [x,y+1]
-			array << [x.prev,y]
-			array << [x,y-1]
+			array << [[x.next,y]]
+			array << [[x,y+1]]
+			array << [[x.prev,y]]
+			array << [[x,y-1]]
 			i = 0
 			6.times do
-				array << [array[i][0].next,y]
-				array << [x,array[i+1][1]+1]
-				array << [array[i+2][0].prev,y]
-				array << [x,array[i+3][1]-1]
-				i += 4
+				array[0] << [array[0][i][0].next,y]
+				array[1] << [x,array[1][i][1]+1]
+				array[2] << [array[2][i][0].prev,y]
+				array[3] << [x,array[3][i][1]-1]
+				i += 1
 			end
 		when :bishop
-			array << [x.next,y+1]
-			array << [x.prev,y+1]
-			array << [x.prev,y-1]
-			array << [x.next,y-1]
+			array << [[x.next,y+1]]
+			array << [[x.prev,y+1]]
+			array << [[x.prev,y-1]]
+			array << [[x.next,y-1]]
 			i = 0
 			6.times do
-				array << [array[i][0].next,array[i][1]+1]
-				array << [array[i+1][0].prev,array[i+1][1]+1]
-				array << [array[i+2][0].prev,array[i+2][1]-1]
-				array << [array[i+3][0].next,array[i+3][1]-1]
-				i += 4
+				array << [array[0][i][0].next,array[0][i][1]+1]
+				array << [array[1][i][0].prev,array[1][i][1]+1]
+				array << [array[2][i][0].prev,array[2][i][1]-1]
+				array << [array[3][i][0].next,array[3][i][1]-1]
+				i += 1
 			end
 		when :knight
-			array << [x.next,y+2]
-			array << [x.next,y-2]
-			array << [x.prev,y+2]
-			array << [x.prev,y-2]
-			array << [x.next.next,y+1]
-			array << [x.next.next,y-1]
-			array << [x.prev.prev,y+1]
-			array << [x.prev.prev,y-1]
+			array << [[x.next,y+2]]
+			array << [[x.next,y-2]]
+			array << [[x.prev,y+2]]
+			array << [[x.prev,y-2]]
+			array << [[x.next.next,y+1]]
+			array << [[x.next.next,y-1]]
+			array << [[x.prev.prev,y+1]]
+			array << [[x.prev.prev,y-1]]
 		when :pawn
 			case @type
 			when :white
-				array << [x,y+1]
-				array << [x.next,y+1]
-				array << [x.prev,y+1]
+				array << [[x,y+1]]
+				array << [[x.next,y+1]]
+				array << [[x.prev,y+1]]
 			when :black
-				array << [x,y-1]
-				array << [x.next,y-1]
-				array << [x.prev,y-1]
+				array << [[x,y-1]]
+				array << [[x.next,y-1]]
+				array << [[x.prev,y-1]]
 			end
 		end
-		array & spaces
+		final_array = []
+		array.each_with_index do |moves,index|
+			final_array[index] = moves & spaces
+		end
+		final_array
 	end
 
 	def spaces
