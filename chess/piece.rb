7a2da0a2b21f2779e082f02cacf0f65540ba5ca6
Shaunty Rashidi <shauntyrashidi@Shaunty-Rashidis-Computer.local>
2015-12-01 17:23:54 -0800

    Create possible moves for Piece objects of type king and queen

--- chess/piece.rb
+++ chess/piece.rb
@@ -35,14 +35,36 @@ class Piece
 		case @type
 		when :king
 			array << [x.next,y]
-			array << [x.next,y.next]
-			array << [x,y.next]
-			array << [x.prev,y.next]
+			array << [x.next,y+1]
+			array << [x,y+1]
+			array << [x.prev,y+1]
 			array << [x.prev,y]
 			array << [x.prev,y-1]
 			array << [x,y-1]
 			array << [x.next,y-1]
+		when :queen
+			array << [x.next,y]
+			array << [x.next,y+1]
+			array << [x,y+1]
+			array << [x.prev,y+1]
+			array << [x.prev,y]
+			array << [x.prev,y-1]
+			array << [x,y-1]
+			array << [x.next,y-1]
+			i = 0
+			6.times do
+				array << [array[i][0].next,y]
+				array << [array[i+1][0].next,array[i+1][1]+1]
+				array << [x,array[i+2][1]+1]
+				array << [array[i+3][0].prev,array[i+3][1]+1]
+				array << [array[i+4][0].prev,y]
+				array << [array[i+5][0].prev,array[i+5][1]-1]
+				array << [x,array[i+6][1]-1]
+				array << [array[i+7][0].next,array[i+7][1]-1]
+				i += 8
+			end
 		end
+		array
 	end
 
 end
