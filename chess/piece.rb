5733af2db48567bd8d14124db78f6f28225fbbb5
Shaunty Rashidi <shauntyrashidi@Shaunty-Rashidis-Computer.local>
2015-12-01 16:21:14 -0800

    Initialize new Piece class with readable unicode view

--- chess/piece.rb
+++ chess/piece.rb
@@ -1,10 +1,30 @@
 class Piece
 
-	attr_reader :type, :color
+	attr_reader :type, :color, :view
 
 	def initialize(type,color)
 		@type = type
 		@color = color
+		@view = case color
+		when :white
+			case type
+			when :king then "\u{2654}"
+			when :queen then "\u{2655}"
+			when :rook then "\u{2656}"
+			when :bishop then "\u{2657}"
+			when :knight then "\u{2658}"
+			when :pawn then "\u{2659}"
+			end
+		when :black
+			case type
+			when :king then "\u{265A}"
+			when :queen then "\u{265B}"
+			when :rook then "\u{265C}"
+			when :bishop then "\u{265D}"
+			when :knight then "\u{265E}"
+			when :pawn then "\u{265F}"
+			end
+		end
 	end
 
 end
